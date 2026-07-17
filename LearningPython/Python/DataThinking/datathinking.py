1e532fa239968dfabe03d39940d76d95f686d32e
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-07-17 15:28:23 +0545

    feat(learning): add LearningPython course materials

---
+++ LearningPython/Python/DataThinking/datathinking.py
@@ -0,0 +1,23 @@
+# Never invest all your data on training your model.
+# We use 80% of our data for training and 20% for testing to ensure the     AI learns real patterns instead of just memorizing answers. The 80% teaches the model, and the untouched 20% acts as a mock exam to prove the AI can handle new information.
+
+
+# Data Thinking:
+# 1. Data Collection
+# 2. Data Review
+# 3. Data Segregration
+# 4. Data Cleaning
+# 5. Data Preparation
+# 6. Ai Algorithm fed in
+
+
+# number = [14, 22, 33, 45, 5, 7, 50, 30]
+number = [10,20,30,40,50]
+new_number = []
+for num in number:
+    new_number.append(num + 5)
+print(new_number)
+
+
+#Heavy claculation with low memory usage:- Numpy
+#To filter data set, duplicates, strongness of data: Pandas
---
+++ LearningPython/Python/functions/functions.py
@@ -0,0 +1,42 @@
+# print("hello falano")
+# print("hello falano1")
+# print("hello falano2")
+# print("hello falano3")
+
+
+# repetation(violates DRY),duplication, unmanaged,not reusable
+# def greet():
+#     print("Bye")
+# greet()
+
+
+# Parameterized
+# def greet(name):
+#     print(f"Hello {name}")
+# greet("Gyanendra")
+
+
+# def sum(num1,num2):
+#     return num1 + num2
+
+# data = sum(5,10)
+# print(data)
+
+
+# def student():
+#     return "Gyanendra", 20, "KTM" #return on tuples
+# data = student()
+# print(data)
+# name,age,address = student()
+# print(name)
+# print(age)
+# print(address)
+
+
+# def say_hello(name="Ram"):
+#     return f"Hello {name}"
+
+
+# print(say_hello("Gyanendra"))
+# print(say_hello())
+
